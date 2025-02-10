0a6eb642708d66f673e09a0a883a773acc2a50fb
vatsal <vatsalmunjpara3@gmail.com>
2025-02-10 14:34:25 +0530

    update

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import reactlogo from './assets/react.svg'
 import vitelogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter } from 'react-router-dom'
 
-
+// componets
 import Navbar from './components/navbar'
 import Header from "./components/header"
 import Login from './components/login'
+
 function App() {
   const [count, setCount] = useState(0)
-
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Header/>
+    },
+    {
+      path:'/login',
+      element:<Login/>
+    }
+  ])
   return (
     <>
       <Navbar/>
