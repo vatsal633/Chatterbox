15f7c54305505b3650219acdd2c41b546b9c3bab
vatsal <vatsalmunjpara3@gmail.com>
2025-02-11 13:27:24 +0530

    update login

--- front-end/src/components/login.jsx
+++ front-end/src/components/login.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
-import Navbar from './navbar'
+import { Link } from 'react-router-dom'
 const login = () => {
+
+  const onsubmitform = ()=>{
+    alert("form submited")
+  }
+
   return (
     <>
-      <div className='m-auto w-[50%] '>
-        <form action="" className='bg-white'>
-          <div className='email-area flex justify-center'>
-            <input type="email" name="" id="" className='w-[65%] text-white bg-black' placeholder='enter the email' />
-          </div>
-          <div className='password-area flex justify-center'>
-            <input type="password" name='' id=''  className='w-[65%] text-white bg-black' placeholder='enter the password' />
-          </div>
+      <div className='wrapper flex min-h-[calc(100vh-64px)] justify-center items-center'>
+         <form action="" className='bg-gradient-to-r from-[#662D8C] to-[#ED1E79] w-[37%] rounded-[7px]'>
+            <div className="form-heading text-6xl text-white text-center">Login</div>
+            <div className='email-area flex justify-center  '>
+              <input type="email"  className='px-[12px] py-[11px] my-6 rounded-[7px] w-[72%] text-black bg-white' name="" id="" placeholder='enter the email'/>
+            </div>
+            <div className='password-area flex justify-center'>
+              <input type="password" className='px-[12px] py-[11px] rounded-[7px] w-[72%] text-black bg-white' name="" id="" placeholder='enter password'/>
+            </div>
 
-          <div className='login flex justify-center'>
-            <button type="button " className='bg-black text-white'>fdgfd</button>
-          </div>
-        </form>
+            <div className="submitarea flex justify-center my-5">
+                <button type='submit' onClick={onsubmitform} className='transition duration-[0s,0.5s] bg-white text-black py-[11px] px-[38px] rounded-[7px] cursor-pointer'>Login</button>
+            </div>
+
+            <div className="extra ">
+                <span className='text-black'>Already Have Any Account?</span>
+              <Link to='/signin' className='text-blue underline'>
+              Sign in
+              </Link>
+            </div>
+         </form>
       </div>
 
     </>
